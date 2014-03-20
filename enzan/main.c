6d7eaf3c9cb1ec79b2f83ac533a32d0549829286
Toshiyuki Masui <masui@pitecan.com>
2014-03-20 10:10:26 +0900

    ...

--- enzan/main.c
+++ enzan/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include "data.h"
 
+int search(char *a[], char *s){
+	int len;
+	for(len=0;a[len];len++);
+	return _search(a,s,0,len);
+}
+
+int _search(char *a[], char *s, int start, int end){
+	if(end - start <= 1){
+		return strcmp(s,a[start]) == 0 ? start : -1;
+	}
+	else {
+		int m = (start + end) / 2;
+		int cmp = strcmp(s,a[m]);
+		if(cmp == 0) return m;
+		else if(cmp > 0) return _search(a,s,m,end);
+		else return _search(a,s,start,m);
+	}
+}
+
 main()
 {
 	int i,j;
@@ -17,6 +36,7 @@ main()
 	}
 	*/
 
+	/*
 	for(i=0;i<nbooks;i++){
 		if(strcmp(isbns[i],"4106100037") == 0){
 			break;
@@ -29,5 +49,11 @@ main()
 		printf("%d\n",shelfinds[j]);
 		printf("%s\n",shelfnames[shelfinds[j]]);
 	}
+	*/
+	printf("%d\n",search(isbns,"4106100037"));
+	printf("%s\n",isbns[search(isbns,"4106100037")]);
+	printf("%d\n",search(shelves,"増井"));
+	printf("%s\n",shelves[search(shelves,"増井")]);
+	printf("%d\n",search(shelves,"asdfasdfxxx"));
 }
 
