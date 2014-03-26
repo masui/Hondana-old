ee24323f55c567ae8672d57509ef6c32ff4919f7
Toshiyuki Masui <masui@pitecan.com>
2014-03-26 13:48:46 +0900

    ...

--- enzan/main.c
+++ enzan/main.c
@@ -21,15 +21,19 @@ typedef enum {
 typedef struct {
 	int ind;
 	float score;
-} Entry;
+} ScoreEntry;
+
+#define max(x,y) ((x) > (y) ? (x) : (y))
+#define min(x,y) ((x) < (y) ? (x) : (y))
 
 int compare(const void *p1, const void *p2)
 {
-	float v1 = ((Entry*)p1)->score;
-	float v2 = ((Entry*)p2)->score;
+	float v1 = ((ScoreEntry*)p1)->score;
+	float v2 = ((ScoreEntry*)p2)->score;
 	return v2 > v1 ? 1 : v1 == v2 ? 0 : -1;
 }
 
+#define SIMILAR_LIMIT 20
 //
 // fl = shelf_books[shelf_ind("増井")];
 // fl = calc(SIMILAR,BOOK,SHELF,fl);
@@ -40,21 +44,38 @@ FreqList *calc(Command command, Kind inputkind, Kind outputkind, FreqList *fl1, 
 	FreqList *result;
 	if(outputkind == BOOK){
 		if(inputkind == SHELF){
-			if(command == SEARCH){
-				return _shelf_books[(*fl1)[0][0]];
+			if(command == ADD){ // 必要なのか?
+				return NULL;
+			}
+			if(command == SIMILAR){
+				return NULL;
 			}
 		}
 		if(inputkind == BOOK){
 			if(command == ADD){
 				return fl_join(fl1,fl2);
 			}
+			if(command == SIMILAR){
+				return NULL;
+			}
 		}
 	}
 	if(outputkind == SHELF){
+		if(inputkind == SHELF){
+			if(command == ADD){
+				return fl_join(fl1,fl2);
+			}
+			if(command == SIMILAR){
+				return NULL;
+			}
+		}
 		if(inputkind == BOOK){
+			if(command == ADD){
+				return NULL;
+			}
 			if(command == SIMILAR){
 				int query_books_length = fl_length(fl1);
-				Entry *entries = (Entry*)alloca(sizeof(Entry)*nshelves);
+				ScoreEntry *entries = (ScoreEntry*)alloca(sizeof(ScoreEntry)*nshelves);
 				for(i=0;i<nshelves;i++){
 					FreqList *books = _shelf_books[i];
 					int books_length = fl_length(books);
@@ -62,10 +83,21 @@ FreqList *calc(Command command, Kind inputkind, Kind outputkind, FreqList *fl1, 
 					entries[i].ind = i;
 					
 				}
-				qsort(entries, nshelves, sizeof(Entry), compare);
-				for(i=0;i<10;i++){
+				qsort(entries, nshelves, sizeof(ScoreEntry), compare);
+				int len = min(nshelves,SIMILAR_LIMIT);
+				FreqList *shelflist = (FreqList*)malloc(sizeof(Freq)*(len+1));
+				for(i=0;i<len;i++){
+					(*shelflist)[i][0] = entries[i].ind;
+					(*shelflist)[i][1] = 1;
+				}
+				(*shelflist)[len][0] = -1;
+				return shelflist;
+
+				/*
+				for(i=0;i<SIMILAR_LIMIT;i++){
 					printf("%d %f %s\n",entries[i].ind,entries[i].score,shelves[entries[i].ind]);
 				}
+				*/
 			}
 		}
 	}
@@ -87,4 +119,8 @@ int main()
 
 	FreqList *list1 = shelf_books("yuco");                 // 'yuco' という本棚の本を取得
 	FreqList *list2 = calc(SIMILAR,BOOK,SHELF,list1,NULL); // flという本リストに似た本棚リストを取得
+	for(i=0;i<fl_length(list2);i++){
+		int ind = (*list2)[i][0];
+		printf("%d %s\n",ind,shelves[ind]);
+	}
 }
